6cfd07bb0aa87d9382365987ea65f4714c1371df
AlyonaMykhalchuk <alyonamyhalchuk@gmail.com>
2020-05-17 15:46:30 +0300

    grid-random

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let mosaic = document.getElementById('mosaic');
-const TOTAL_RECTANGLES_AMOUNT = 10;
-let list_amount_rectangles = TOTAL_RECTANGLES_AMOUNT;
+const TOTAL_RECTANGLES_AMOUNT = getRandomIntInclusive(9, 20);
+console.log('TOTAL_RECTANGLES_AMOUNT', TOTAL_RECTANGLES_AMOUNT);
+let list_amount_rectangles = TOTAL_RECTANGLES_AMOUNT - 1;
 let amount_horizontal_blocks = 400;
 let amount_vertical_blocks = 100;
 let cells_amount = 40000;
@@ -10,8 +11,6 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
-
-console.log(TOTAL_RECTANGLES_AMOUNT);
 
 function create_rectangles(num) {
     for (let i = 0; i < num; i++) {
@@ -31,22 +30,26 @@ function create_rectangles(num) {
 create_rectangles(TOTAL_RECTANGLES_AMOUNT - 1);
 
 function set_size(div) {
-    let vertical_blocks = getRandomIntInclusive(1, count_max_verical_blocks_amount_limit());
-    let horizontal_blocks = getRandomIntInclusive(1, count_max_horizontal_blocks_amount_limit());
-    let square = vertical_blocks * horizontal_blocks;
-    cells_amount -= square;
-    list_amount_rectangles -= 1;
-    amount_horizontal_blocks -= horizontal_blocks;
-    amount_vertical_blocks -= vertical_blocks;
-    div.style.gridColumn = `auto / span ${vertical_blocks}`;
-    div.style.gridRow = `auto / span ${horizontal_blocks}`;
+    let square_limit = cells_amount - list_amount_rectangles;  // рахуємо максимальну кількість ячейок яку МОЖЕ займати ОДИН прямокутник
+    // так щоб залишилось хоча б по 1 ячейці на кожного
+    console.log('square_limit', square_limit);
+    list_amount_rectangles -= 1; //зменшуємо кількість прямокутників які ще потрібно намалювати
+    let square_div = Math.round(getRandomIntInclusive(1, square_limit));//визначаємо рандомно фактичну площу 1-го прямокутника
+    console.log('square_div', square_div);
+    cells_amount -= square_div;//віднімаємо від загальної к-сті ячейок використані ячейки на вже намальований прямокутник
+    console.log('cells_amount', cells_amount);
+    let height_div = Math.round(square_div / getRandomIntInclusive(1, amount_horizontal_blocks));// ділимо площу прямокутника(яку знайшли рандомно)
+                                                                                                      // на рандомну ширину
+    console.log('height_div', height_div);
+    let width_div = Math.round(square_div / height_div);
+    console.log('width_div', width_div);
+    amount_horizontal_blocks -= width_div; // визначаємо скільки залишилось вільних стовпчиків
+    console.log('amount_horizontal_blocks', amount_horizontal_blocks);
+    amount_vertical_blocks -= height_div; // визначаємо скільки залишилось вільних рядків
+    console.log(' amount_vertical_blocks', amount_vertical_blocks);
+
+    div.style.gridColumn = `auto / span ${width_div}`;
+    div.style.gridRow = `auto / span ${height_div}`;
 
 }
 
-function count_max_verical_blocks_amount_limit() {
-    return amount_vertical_blocks - list_amount_rectangles;
-}
-
-function count_max_horizontal_blocks_amount_limit() {
-    return amount_horizontal_blocks - list_amount_rectangles;
-}
